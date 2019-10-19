400c67563e9033afbeef9cf9140813575582ca62
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-19 20:18:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # Your Code Here
+
+def map(source_array)
+    new_array = []
+    i= 0 
+    while i < source_array.length do
+      new_array.push(yield(source_array[i]))
+      i += 1
+    end
+    map(source_array){|n|n * -1}
+end
+  
