bbefcf25ce4b7bfdee405b9831110c6ed698f9b5
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-19 20:47:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -16,8 +16,8 @@ def reduce(source_array, starting_point = nil)
   if starting_point
     total = starting_point
     i= 0
-  else
-    total = source_array[i]
+  elsel
+    total = source_array[0]
     i= 0
   end
   while i < source_array.length do
